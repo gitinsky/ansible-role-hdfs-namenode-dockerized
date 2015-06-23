7ea1e62c361a58a5bb5867e26714d34f82d2de7e
Alex Chistyakov <alexclear@gmail.com>
2015-06-24 02:57:29 +0300

    Use a separate volume for logs

--- templates/start.sh
+++ templates/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 chown -R hadoop:hadoop /var/hadoop
+chown -R hadoop:hadoop /var/log/hdfs
 if [ ! -e /var/hadoop/dfs ]; then
    sudo -E -u hadoop /hadoop/bin/hdfs namenode -format
 fi
-exec sudo -E -u hadoop /hadoop/bin/hdfs namenode 1>>/var/log/hdfs-namenode.log 2>&1
+exec sudo -E -u hadoop /hadoop/bin/hdfs namenode 1>>/var/log/hdfs/namenode.log 2>&1
